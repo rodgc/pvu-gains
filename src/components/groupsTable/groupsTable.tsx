9bb8ffd733b2028341f935625782a6c6992be2b7
Rodrigo Gonzalez <rodrigo.gonzalez@levvel.io>
2021-08-10 00:38:01 -0500

    Add Highligth current group hour

--- src/components/groupsTable/groupsTable.tsx
+++ src/components/groupsTable/groupsTable.tsx
@@ -1,13 +1,21 @@
 import React, { Fragment } from 'react'
 import db from '../../firebase'
+import { useStyles } from './styles'
 
 function convertTime(time: number) {
   const date = new Date(time * 1000)
   return `${date.getHours()}:${date.getMinutes()}`
 }
 
+function isActive(time: number) {
+  const currentDate = new Date()
+  const resetDate = new Date(time * 1000)
+  return currentDate.getHours() === resetDate.getHours()
+}
+
 const GroupsTable: React.FC = () => {
   const [groups, setGroups] = React.useState<any[]>([])
+  const classes = useStyles()
 
   React.useEffect(() => {
     db.collection('groups').onSnapshot((snapshot) =>
@@ -34,12 +42,36 @@ const GroupsTable: React.FC = () => {
           {groups.map((group) => (
             <tr key={group.name}>
               <td>{group.name}</td>
-              <td>{convertTime(group.time.seconds)}</td>
-              <td>{convertTime(group.time2.seconds)}</td>
-              <td>{convertTime(group.time3.seconds)}</td>
-              <td>{convertTime(group.time4.seconds)}</td>
-              <td>{convertTime(group.time5.seconds)}</td>
-              <td>{convertTime(group.time6.seconds)}</td>
+              <td
+                className={isActive(group.time.seconds) ? classes.active : ''}
+              >
+                {convertTime(group.time.seconds)}
+              </td>
+              <td
+                className={isActive(group.time2.seconds) ? classes.active : ''}
+              >
+                {convertTime(group.time2.seconds)}
+              </td>
+              <td
+                className={isActive(group.time3.seconds) ? classes.active : ''}
+              >
+                {convertTime(group.time3.seconds)}
+              </td>
+              <td
+                className={isActive(group.time4.seconds) ? classes.active : ''}
+              >
+                {convertTime(group.time4.seconds)}
+              </td>
+              <td
+                className={isActive(group.time5.seconds) ? classes.active : ''}
+              >
+                {convertTime(group.time5.seconds)}
+              </td>
+              <td
+                className={isActive(group.time6.seconds) ? classes.active : ''}
+              >
+                {convertTime(group.time6.seconds)}
+              </td>
             </tr>
           ))}
         </tbody>
---
+++ src/components/groupsTable/styles.ts
@@ -0,0 +1,7 @@
+import { makeStyles } from '@material-ui/core'
+
+export const useStyles = makeStyles((theme) => ({
+  active: {
+    color: theme.palette.secondary.main,
+  },
+}))
